07a7b4ade5f85217f4238db1a7b6211c5010967c
ry <ry@localhost.localdomain>
2019-03-18 15:30:48 -0400

    upgraded storybook to 5.0.3, jest, enzyme...

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import 'regenerator-runtime/runtime'
-import { configure, addDecorator, storiesOf } from '@storybook/react'
-import { themes } from '@storybook/components'
-import { withOptions } from '@storybook/addon-options'
+import {
+  configure,
+  addDecorator,
+  addParameters,
+  storiesOf
+} from '@storybook/react'
 import { withKnobs } from '@storybook/addon-knobs'
 
 import theme from '../src/theme'
@@ -16,29 +19,27 @@ function loadStories () {
 }
 
 // storybook options
-addDecorator(
-  withOptions({
-    name: 'ui-workbench',
-    url: 'http://drdyne.github.io/ui-workbench',
-    hierarchySeparator: /\//,
-    hierarchyRootSeparator: /\|/,
+addParameters({
+  options: {
+    brandTitle: 'ui-workbench',
+    brandUrl: 'http://drdyne.github.io/ui-workbench',
     sortStoriesByKind: true,
-    enableShortcuts: false,
-    theme: {
-      ...themes.light,
-      mainBorder: '1px solid transparent',
-      mainBorderRadius: '0',
-      mainBackground: '#fff',
-      mainTextFace: 'Roboto Mono,Montserrat,Helvetica,Arial,sans-serif',
-      mainTextColor: '#444',
-      dimmedTextColor: '#ccc',
-      highlightColor: '#ea4862', // '#331C86',
-      inputFill: 'rgba(0,0,0,0.05)',
-      barFill: 'rgba(43, 43, 43, 0.05)',
-      menuLink: { color: '#331c86', fontWeight: 'bold' }
-    }
-  })
-)
+    enableShortcuts: false
+    //    theme: {
+    //      ...themes.light,
+    //      mainBorder: '1px solid transparent',
+    //      mainBorderRadius: '0',
+    //      mainBackground: '#fff',
+    //      mainTextFace: 'Roboto Mono,Montserrat,Helvetica,Arial,sans-serif',
+    //      mainTextColor: '#444',
+    //      dimmedTextColor: '#ccc',
+    //      highlightColor: '#ea4862', // '#331C86',
+    //      inputFill: 'rgba(0,0,0,0.05)',
+    //      barFill: 'rgba(43, 43, 43, 0.05)',
+    //      menuLink: { color: '#331c86', fontWeight: 'bold' }
+    //    }
+  }
+})
 
 // addons
 addDecorator(withKnobs)
